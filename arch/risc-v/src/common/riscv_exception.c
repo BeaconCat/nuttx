lupyuen2/wip-nuttx

riscv: Implement page-fault exception and on-demand paging
When an application is being loaded `up_addrenv_create ` calls
`create_region` to create the address environment. Only the first
entry is mapped when the region is created. Virtual memory that is
not mapped will trigger an exception when accessed. Other memory
pages are allocated and mapped on-demand. This enables setting
larger heap and stack for the process without compromising the
overall system memory.

arch/risc-v/src/common/riscv_exception.c
@@ -30,6 +30,14 @@
 
 #include <nuttx/irq.h>
 #include <nuttx/arch.h>
+#ifdef CONFIG_PAGING
+#include <nuttx/pgalloc.h>
+#endif
+
+#ifdef CONFIG_PAGING
+#include "pgalloc.h"
+#include "riscv_mmu.h"
+#endif
 
 #include "riscv_internal.h"
 #include "chip.h"
@@ -88,6 +96,116 @@ int riscv_exception(int mcause, void *regs, void *args)
 }
 
 /****************************************************************************
+ * Name: riscv_fillpage
+ *
+ * Description:
+ *   This function is an exception handler for page faults in a RISC-V.
+ *   It is invoked when a page fault exception occurs, which is typically
+ *   when a process tries to access a page that is not currently in memory.
+ *
+ *   The function takes as arguments the machine cause (mcause) which
+ *   indicates the cause of the exception, a pointer to the register state
+ *   at the time of the exception (regs), and a pointer to any additional
+ *   arguments (args).
+ *
+ *   The function should handle the exception appropriately, typically by
+ *   loading the required page into memory and updating the page table.
+ *
+ * Input Parameters:
+ *   mcause - The machine cause of the exception.
+ *   regs   - A pointer to the register state at the time of the exception.
+ *   args   - A pointer to any additional arguments.
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_PAGING
+int riscv_fillpage(int mcause, void *regs, void *args)
+{
+  uintptr_t cause = mcause & RISCV_IRQ_MASK;
+  uintptr_t ptlast;
+  uintptr_t ptprev;
+  uintptr_t paddr;
+  uintptr_t vaddr;
+  uint32_t  ptlevel;
+  uintptr_t satp;
+  uint32_t mmuflags;
+
+  _info("EXCEPTION: %s. MCAUSE: %" PRIxREG ", EPC: %" PRIxREG
+        ", MTVAL: %" PRIxREG "\n",
+        mcause > RISCV_MAX_EXCEPTION ? "Unknown" : g_reasons_str[cause],
+        cause, READ_CSR(CSR_EPC), READ_CSR(CSR_TVAL));
+
+  satp    = READ_CSR(CSR_SATP);
+  vaddr   = MM_PGALIGNDOWN(READ_CSR(CSR_TVAL));
+  ptprev  = riscv_pgvaddr(mmu_satp_to_paddr(satp));
+  ptlevel = ARCH_SPGTS;
+
+  paddr = mmu_pte_to_paddr(mmu_ln_getentry(ptlevel, ptprev, vaddr));
+
+  if (!paddr)
+    {
+      /* Nothing yet, allocate one page for final level page table */
+
+      paddr = mm_pgalloc(1);
+      if (!paddr)
+        {
+          return -ENOMEM;
+        }
+
+      /* Map the page table to the prior level */
+
+      mmu_ln_setentry(ptlevel, ptprev, paddr, vaddr, MMU_UPGT_FLAGS);
+
+      /* This is then used to map the final level */
+
+      riscv_pgwipe(paddr);
+    }
+
+  ptlast = riscv_pgvaddr(paddr);
+
+  paddr = mm_pgalloc(1);
+  if (!paddr)
+    {
+      return -ENOMEM;
+    }
+
+  /* Wipe the physical page memory */
+
+  riscv_pgwipe(paddr);
+
+  if (vaddr >= CONFIG_ARCH_TEXT_VBASE && vaddr <= ARCH_TEXT_VEND)
+    {
+      mmuflags = MMU_UTEXT_FLAGS;
+      mmuflags |= PTE_W;
+    }
+  else if (vaddr >= CONFIG_ARCH_DATA_VBASE && vaddr <= ARCH_DATA_VEND)
+    {
+      mmuflags = MMU_UDATA_FLAGS;
+    }
+  else if (vaddr >= CONFIG_ARCH_HEAP_VBASE && vaddr <= ARCH_HEAP_VEND)
+    {
+      mmuflags = MMU_UDATA_FLAGS;
+    }
+  else
+    {
+      _alert("PANIC!!! virtual address not mappable: %" PRIxPTR "\n", vaddr);
+      up_irq_save();
+      CURRENT_REGS = regs;
+      PANIC_WITH_REGS("panic", regs);
+    }
+
+  /* Then map the virtual address to the physical address */
+
+  mmu_ln_setentry(ptlevel + 1, ptlast, paddr, vaddr, mmuflags);
+
+  return 0;
+}
+#endif /* CONFIG_PAGING */
+
+/****************************************************************************
  * Name: riscv_exception_attach
  *
  * Description:
@@ -130,9 +248,16 @@ void riscv_exception_attach(void)
 #endif
 
   irq_attach(RISCV_IRQ_INSTRUCTIONPF, riscv_exception, NULL);
+
+#ifdef CONFIG_PAGING
+  irq_attach(RISCV_IRQ_LOADPF, riscv_fillpage, NULL);
+  irq_attach(RISCV_IRQ_STOREPF, riscv_fillpage, NULL);
+#else
   irq_attach(RISCV_IRQ_LOADPF, riscv_exception, NULL);
-  irq_attach(RISCV_IRQ_RESERVED, riscv_exception, NULL);
   irq_attach(RISCV_IRQ_STOREPF, riscv_exception, NULL);
+#endif
+
+  irq_attach(RISCV_IRQ_RESERVED, riscv_exception, NULL);
 
 #ifdef CONFIG_SMP
   irq_attach(RISCV_IRQ_SOFT, riscv_pause_handler, NULL);

BeaconCat/nuttx

arch/risc-v/src/common/riscv_exception.c
@@ -30,6 +30,14 @@
 
 #include <nuttx/irq.h>
 #include <nuttx/arch.h>
+#ifdef CONFIG_PAGING
+#  include <nuttx/pgalloc.h>
+#endif
+
+#ifdef CONFIG_PAGING
+#  include "pgalloc.h"
+#  include "riscv_mmu.h"
+#endif
 
 #include "riscv_internal.h"
 #include "chip.h"
@@ -88,6 +96,117 @@ int riscv_exception(int mcause, void *regs, void *args)
 }
 
 /****************************************************************************
+ * Name: riscv_fillpage
+ *
+ * Description:
+ *   This function is an exception handler for page faults in a RISC-V.
+ *   It is invoked when a page fault exception occurs, which is typically
+ *   when a process tries to access a page that is not currently in memory.
+ *
+ *   The function takes as arguments the machine cause (mcause) which
+ *   indicates the cause of the exception, a pointer to the register state
+ *   at the time of the exception (regs), and a pointer to any additional
+ *   arguments (args).
+ *
+ *   The function should handle the exception appropriately, typically by
+ *   loading the required page into memory and updating the page table.
+ *
+ * Input Parameters:
+ *   mcause - The machine cause of the exception.
+ *   regs   - A pointer to the register state at the time of the exception.
+ *   args   - A pointer to any additional arguments.
+ *
+ * Returned Value:
+ *   Zero (OK) on success; a negated errno value on failure.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_PAGING
+int riscv_fillpage(int mcause, void *regs, void *args)
+{
+  uintptr_t cause = mcause & RISCV_IRQ_MASK;
+  uintptr_t ptlast;
+  uintptr_t ptprev;
+  uintptr_t paddr;
+  uintptr_t vaddr;
+  uint32_t  ptlevel;
+  uintptr_t satp;
+  uint32_t mmuflags;
+
+  _info("EXCEPTION: %s. MCAUSE: %" PRIxREG ", EPC: %" PRIxREG
+        ", MTVAL: %" PRIxREG "\n",
+        mcause > RISCV_MAX_EXCEPTION ? "Unknown" : g_reasons_str[cause],
+        cause, READ_CSR(CSR_EPC), READ_CSR(CSR_TVAL));
+  vaddr = MM_PGALIGNDOWN(READ_CSR(CSR_TVAL));
+  if (vaddr >= CONFIG_ARCH_TEXT_VBASE && vaddr <= ARCH_TEXT_VEND)
+    {
+      mmuflags = MMU_UTEXT_FLAGS;
+
+      /* Write access to .text region needs to be set according to
+       * https://github.com/apache/nuttx/pull/6193.
+       */
+
+      mmuflags |= PTE_W;
+    }
+  else if (vaddr >= CONFIG_ARCH_DATA_VBASE && vaddr <= ARCH_DATA_VEND)
+    {
+      mmuflags = MMU_UDATA_FLAGS;
+    }
+  else if (vaddr >= CONFIG_ARCH_HEAP_VBASE && vaddr <= ARCH_HEAP_VEND)
+    {
+      mmuflags = MMU_UDATA_FLAGS;
+    }
+  else
+    {
+      _alert("PANIC!!! virtual address not mappable: %" PRIxPTR "\n", vaddr);
+      up_irq_save();
+      CURRENT_REGS = regs;
+      PANIC_WITH_REGS("panic", regs);
+    }
+
+  satp    = READ_CSR(CSR_SATP);
+  ptprev  = riscv_pgvaddr(mmu_satp_to_paddr(satp));
+  ptlevel = ARCH_SPGTS;
+  paddr = mmu_pte_to_paddr(mmu_ln_getentry(ptlevel, ptprev, vaddr));
+  if (!paddr)
+    {
+      /* Nothing yet, allocate one page for final level page table */
+
+      paddr = mm_pgalloc(1);
+      if (!paddr)
+        {
+          return -ENOMEM;
+        }
+
+      /* Map the page table to the prior level */
+
+      mmu_ln_setentry(ptlevel, ptprev, paddr, vaddr, MMU_UPGT_FLAGS);
+
+      /* This is then used to map the final level */
+
+      riscv_pgwipe(paddr);
+    }
+
+  ptlast = riscv_pgvaddr(paddr);
+  paddr = mm_pgalloc(1);
+  if (!paddr)
+    {
+      return -ENOMEM;
+    }
+
+  /* Wipe the physical page memory */
+
+  riscv_pgwipe(paddr);
+
+  /* Then map the virtual address to the physical address */
+
+  mmu_ln_setentry(ptlevel + 1, ptlast, paddr, vaddr, mmuflags);
+
+  return 0;
+}
+#endif /* CONFIG_PAGING */
+
+/****************************************************************************
  * Name: riscv_exception_attach
  *
  * Description:
@@ -130,9 +249,16 @@ void riscv_exception_attach(void)
 #endif
 
   irq_attach(RISCV_IRQ_INSTRUCTIONPF, riscv_exception, NULL);
+
+#ifdef CONFIG_PAGING
+  irq_attach(RISCV_IRQ_LOADPF, riscv_fillpage, NULL);
+  irq_attach(RISCV_IRQ_STOREPF, riscv_fillpage, NULL);
+#else
   irq_attach(RISCV_IRQ_LOADPF, riscv_exception, NULL);
-  irq_attach(RISCV_IRQ_RESERVED, riscv_exception, NULL);
   irq_attach(RISCV_IRQ_STOREPF, riscv_exception, NULL);
+#endif
+
+  irq_attach(RISCV_IRQ_RESERVED, riscv_exception, NULL);
 
 #ifdef CONFIG_SMP
   irq_attach(RISCV_IRQ_SOFT, riscv_pause_handler, NULL);