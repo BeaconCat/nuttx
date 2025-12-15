LingaoM/incubator-nuttx

bugfix:recheck task status when reboot task_fssync
O62MWT-2829

Change-Id: I11890beaa5402787fd991a819d62b0037cce0eaa
Signed-off-by: guohao15 <guohao15@xiaomi.com>

fs/inode/fs_files.c
@@ -349,9 +349,16 @@ static int nx_dup3_from_tcb(FAR struct tcb_s *tcb, int fd1, int fd2,
  *
  ****************************************************************************/
 
-void files_initlist(FAR struct filelist *list)
+int files_initlist(FAR struct filelist **list)
 {
-  list->fl_crefs = 1;
+  *list = kmm_zalloc(sizeof(struct filelist));
+  if (*list == NULL)
+    {
+      return -ENOMEM;
+    }
+
+  (*list)->fl_crefs = 1;
+  return OK;
 }
 
 /****************************************************************************
@@ -425,7 +432,7 @@ FAR struct filelist *files_getlist(FAR struct tcb_s *tcb)
 
   if (tcb->group != NULL)
     {
-      list = &tcb->group->tg_filelist;
+      list = tcb->group->tg_filelist;
       if (list->fl_crefs > 0)
         {
           list->fl_crefs++;
@@ -475,6 +482,7 @@ void files_putlist(FAR struct filelist *list)
     }
 
   kmm_free(list->fl_files);
+  kmm_free(list);
 }
 
 /****************************************************************************

BeaconCat/nuttx

fs/inode/fs_files.c
@@ -361,9 +361,16 @@ static int nx_dup3_from_tcb(FAR struct tcb_s *tcb, int fd1, int fd2,
  *
  ****************************************************************************/
 
-void files_initlist(FAR struct filelist *list)
+int files_initlist(FAR struct filelist **list)
 {
-  list->fl_crefs = 1;
+  *list = kmm_zalloc(sizeof(struct filelist));
+  if (*list == NULL)
+    {
+      return -ENOMEM;
+    }
+
+  (*list)->fl_crefs = 1;
+  return OK;
 }
 
 /****************************************************************************
@@ -455,7 +462,7 @@ FAR struct filelist *files_getlist(FAR struct tcb_s *tcb)
 
   if (tcb->group != NULL)
     {
-      list = &tcb->group->tg_filelist;
+      list = tcb->group->tg_filelist;
       if (list->fl_crefs > 0)
         {
           list->fl_crefs++;
@@ -505,6 +512,7 @@ void files_putlist(FAR struct filelist *list)
     }
 
   fs_heap_free(list->fl_files);
+  fs_heap_free(list);
 }
 
 /****************************************************************************