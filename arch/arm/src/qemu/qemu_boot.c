LingaoM/incubator-nuttx

arm: support reboot / poweroff for qemu virt arm
VELAPLATFO-13528

Change-Id: I363bd4db0bd863e8162eef4bd3f67124f891444a
Signed-off-by: Shanmin Zhang <zhangshanmin@xiaomi.com>

arch/arm/src/qemu/qemu_boot.c
@@ -51,6 +51,10 @@ void arm_boot(void)
 
   qemu_setupmappings();
 
+#if defined(CONFIG_ARCH_HAVE_PSCI)
+  arm_psci_init("smc");
+#endif
+
 #ifdef CONFIG_DEVICE_TREE
   fdt_register((FAR const char *)0x40000000);
 #endif

BeaconCat/nuttx

arch/arm/src/qemu/qemu_boot.c
@@ -51,6 +51,10 @@ void arm_boot(void)
 
   qemu_setupmappings();
 
+#if defined(CONFIG_ARCH_HAVE_PSCI)
+  arm_psci_init("hvc");
+#endif
+
 #ifdef CONFIG_DEVICE_TREE
   fdt_register((FAR const char *)0x40000000);
 #endif